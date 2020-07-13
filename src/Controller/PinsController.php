fa3be9bec388db7ac313b164f0609ef28a8f48cb
manhal abdulraazak <cda19-ama@ccicampus.fr>
2020-07-13 07:29:50 +0200

    testing!!

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -3,27 +3,33 @@
 namespace App\Controller;
 
 use App\Entity\Pin;
+use App\Repository\PinRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\ORM\EntityManagerInterface;
-use Twig\Environment;
 
 class PinsController extends AbstractController
 {
 
     /**
-     * @Route("/", name="pins")
+     * @Route("/", name="app_home")
      */
-    public function index(EntityManagerInterface $em) : Response
+    public function index(PinRepository $pinRepository): Response
 {
-    $pin = new Pin;
-    $pin->setTitle("title 3");
-    $pin->setDescription('Description 3');
-
-    $this->getDoctrine()->getManager();
-    dump($pin);
+    $pin = $pinRepository->findAll();
 
     return $this->render('Pins/index.html.twig');
+
 }
+
+    /**
+     * @Route("/pins/{id<[0-9] +>}", name="app_pins_show")
+     * @return Response
+     */
+    public function show(Pin $pin): Response
+    {
+        dd($pin);
+
+    }
 }
+
